be82ed1e19d58368dba3c72116bf30cede75e15d
Alle_rayane <alequissandrarayane123@gmail.com>
2022-12-01 23:54:19 -0300

    Ateração 2.0

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from "./styles";
 import styles from "./login.module.css";
 import { Outlet, Link } from "react-router-dom";
+import imgLogin from '../../assets/imgLogin.png';
 
 function Login(){
 
@@ -18,18 +19,23 @@ function Login(){
 
    return <Container>
  
-        <h3>To Do List APP</h3>
-            <br></br>
-            <div className={styles.designInferiorDireito} ></div>
-            <div className={styles.designInferiorEsquerdo} ></div>
+                <div className={styles.titulo}>
+                    <div className={styles.imgLogin}><img src={imgLogin} alt=""/></div>
+                    <h1 >TO DO LIST APP</h1>
+                </div>
+                <br></br>
+                <div className={styles.designInferiorDireito} ></div>
+                <div className={styles.designInferiorEsquerdo} ></div>
+
             <form >
                 <input className={styles.dados} id="email" type="e-mail" name="email" placeholder="E-mail" ></input>
                 <input className={styles.dados} id="senha" type="password" name="senha" placeholder="Senha" ></input>
                 <button className={styles.botaoEnviar} type="submit" name="login"  value="" onClick={Enviar} >Login</button>   
                 <label className={styles.manterConectado}> <input className={styles.manterConectado} type="checkbox"></input> Mantenha-me conectado</label>
             </form>
+
             <div className={styles.configuracaoConta}>
-                    <Link className={styles.esqueceuSenha} to="/">Esqueceu a senha?</Link>
+                    <Link className={styles.esqueceuSenha} to="/esqueceuSenha">Esqueceu a senha?</Link>
                     <Link className={styles.novaConta} to="/register">Criar nova conta</Link>
             </div>
             
--- src/pages/Register/register.js
+++ src/pages/Register/register.js
@@ -2,10 +2,11 @@ import {Container} from './styles';
 import styles from "./register.module.css";
 import { Outlet, Link } from "react-router-dom";
 import { FcPrevious } from "react-icons/fc";
-
+import {FaUserCircle} from 'react-icons/fa';
+import imgCadastro from '../../assets/imgRegister.png';
 
 function Adoption(){
-
+   
    function Registrar(){
       const email = document.getElementById('email').value;
       const senha = document.getElementById('senha').value;
@@ -20,25 +21,25 @@ function Adoption(){
   }
 
    return( <Container>
+         
          <div className={styles.designInferiorDireito} ></div>
-         <div className={styles.iconVoltar} >
-            <Link to="/Login"> <FcPrevious/></Link> 
+            <div className={styles.iconVoltar} >
+               <Link to="/Login"> <FcPrevious/></Link> 
          </div>
-         
          <div className={styles.titulo}>
-         <h2 >CRIAR NOVA CONTA</h2>
+            <div className={styles.user}><FaUserCircle /></div>
+            <h2 >CRIAR NOVA CONTA</h2>
          </div>
-         
-
-         <br/>
-
+            <br/>
          <form >
                <input className={styles.dados} id="nome" type="name" name="nome" placeholder="Nome" ></input>
                <input className={styles.dados} id="email" type="e-mail" name="email" placeholder="E-mail" ></input>
                <input className={styles.dados} id="senha" type="password" name="senha" placeholder="Criar Senha" ></input>
                <button className={styles.botaoEnviar} type="submit" name="login"  value="" onClick={Registrar}> Registrar</button>   
-               
          </form>
+         
+         <img src={imgCadastro} alt=""/>
+
          <Outlet />
    </Container>);
 }
